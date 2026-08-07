c16d0ed9ce08f0fb74fc4c998e4dbca1f6458f51
VaninaCodes <vannilaresquin@gmail.com>
2026-08-07 10:44:43 -0300

    feat(config): se llamaron a las variables y se conecto a la base de datos

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,17 @@
 import { Sequelize } from "sequelize";
+// .env
+import dotenv from "dotenv";
+dotenv.config();
 
-export const sequelize = new Sequelize("tasks_users_db", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-});
+export const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
 
 export const startDB = async () => {
     try {
@@ -15,3 +23,5 @@ export const startDB = async () => {
         console.error("No se pudo conectar a la base de datos: ", error);
     }
 };
+
+export default sequelize;
