5bc39a1f8269f0bd89db45fa2da9d2a24d65c3fc
VaninaCodes <Vannilaresquin@gmail.com>
2026-08-17 17:54:46 -0300

    refactor(api): se refactorizo el task.controller incluyendo las relaciones

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,8 +1,29 @@
+import { perfilModel } from "../models/perfil.model.js";
 import { taskModel } from "../models/task.model.js";
+import { userModel } from "../models/user.model.js";    
 
 export const getAllTask = async (req, res) => {
     try{
-        const task = await taskModel.findAll();
+        const task = await taskModel.findAll({
+            attributes: {
+                exclude: ["user_id"],
+            },
+            include: [ 
+                {
+                    model: userModel, 
+                    as: "author", 
+                    attributes: {
+                        exclude:[password],
+                    },
+                include: [
+                    {
+                        model: perfilModel,
+                        as: "owner",
+                    }
+                    ] 
+                }
+             ]
+        });
 
         return res.status(200).json(task);
     }
@@ -14,7 +35,26 @@ export const getAllTask = async (req, res) => {
 
 export const getTaskById = async (req, res) => {
     try{
-        const task = await taskModel.findByPk(req.params.id);
+        const task = await taskModel.findByPk(req.params.id, {
+            attributes: {
+                exclude: ["user_id"],
+            },
+            include: [ 
+                {
+                    model: userModel, 
+                    as: "author", 
+                    attributes: {
+                        exclude:[password],
+                    },
+                include: [
+                    {
+                        model: perfilModel,
+                        as: "owner",
+                    }
+                    ] 
+                }
+             ]
+        });
         //validacion
         if (task) res.json(task);
         else res.status(404).json({message: "Tarea no encontrada"});
@@ -29,9 +69,16 @@ export const createTask = async (req, res) => {
     try{
         const { title, description, user_id } = req.body;
         // validacion
-        if (!title || !description){
-            return res.status.json({message: "Debe completar los campos titulo y descripcion"})
+        if (!title || !description || !user_id){
+            return res.status(404).json({message: "Debe completar los campos titulo y descripcion"})
         }
+        
+        // validacion para no crear tarea sin usuario
+        const userExists = await userModel.findByPk(user_id);
+        if(!userExists){
+            return res.status(404).json({message: "No se puede crear la tarea: el usuario no existe"})
+        }
+
         const newTask = await taskModel.create({
             title,
             description,
